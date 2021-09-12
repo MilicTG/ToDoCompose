3110efdf407d44f1dfbcac5883f52c7b8f7cee76
MilicTG <milic.tg@gmail.com>
2021-09-12 20:34:08 +0200

    Completed video 14

--- app/src/main/java/dev/milic/to_docompose/MainActivity.kt
+++ app/src/main/java/dev/milic/to_docompose/MainActivity.kt
@@ -5,11 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import dev.milic.to_docompose.ui.theme.ToDoComposeTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
