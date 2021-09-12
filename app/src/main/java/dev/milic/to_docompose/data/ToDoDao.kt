1d50f10fff8ec80e996af88859c49f96a39437b0
MilicTG <milic.tg@gmail.com>
2021-09-12 16:24:27 +0200

    Broken internet

--- app/src/main/java/dev/milic/to_docompose/data/ToDoDao.kt
+++ app/src/main/java/dev/milic/to_docompose/data/ToDoDao.kt
@@ -1,8 +1,16 @@
 package dev.milic.to_docompose.data
 
 import androidx.room.Dao
+import androidx.room.Query
+import dev.milic.to_docompose.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ToDoDao {
-    
+
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+    fun getAllTasks(): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table WHERE id=:taskId")
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask>
 }
