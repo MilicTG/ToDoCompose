274329fed8f158591fb848b26fa39fbeff79c2c4
MilicTG <milic.tg@gmail.com>
2021-09-16 09:13:08 +0200

    Video 22 first 12 minutes

--- app/src/main/java/dev/milic/to_docompose/navigation/destinations/ListComposable.kt
+++ app/src/main/java/dev/milic/to_docompose/navigation/destinations/ListComposable.kt
@@ -9,7 +9,7 @@ import dev.milic.to_docompose.util.Constants.LIST_ARGUMENT_KEY
 import dev.milic.to_docompose.util.Constants.LIST_SCREEN
 
 fun NavGraphBuilder.listComposable(
-    navigateToTaskScreen: (Int) -> Unit
+    navigateToTaskScreen: (taskId: Int) -> Unit
 ) {
     composable(
         route = LIST_SCREEN,
--- app/src/main/java/dev/milic/to_docompose/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/dev/milic/to_docompose/ui/screens/list/ListAppBar.kt
@@ -1,19 +1,23 @@
 package dev.milic.to_docompose.ui.screens.list
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import dev.milic.to_docompose.ui.theme.topAppBarBackgroundColor
 import dev.milic.to_docompose.ui.theme.topAppBarContentColor
+import dev.milic.to_docompose.R
+import dev.milic.to_docompose.data.models.Priority
 
 @Composable
 fun ListAppBar() {
-    DefaultListAppBar()
+    DefaultListAppBar(onSearchClicked = {})
 }
 
 @Composable
-fun DefaultListAppBar() {
+fun DefaultListAppBar(onSearchClicked: () -> Unit) {
     TopAppBar(
         title = {
             Text(
@@ -21,6 +25,35 @@ fun DefaultListAppBar() {
                 color = MaterialTheme.colors.topAppBarContentColor
             )
         },
+        actions = {
+            ListAppBarActions(onSearchClicked = onSearchClicked)
+        },
         backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor
     )
+}
+
+@Composable
+fun ListAppBarActions(onSearchClicked: () -> Unit) {
+    SearchAction(onSearchClicked = onSearchClicked)
+}
+
+@Composable
+fun SearchAction(onSearchClicked: () -> Unit) {
+    IconButton(onClick = { onSearchClicked }) {
+        Icon(
+            imageVector = Icons.Filled.Search,
+            contentDescription = stringResource(id = R.string.search_task),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+    }
+}
+
+@Composable
+fun SortAction(onSortClicked: (Priority) -> Unit) {
+    IconButton(onClick = { /*TODO*/ }) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_filter_list),
+            contentDescription = stringResource(id = R.string.sort_task)
+        )
+    }
 }
