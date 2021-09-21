5a03b20ea972b315ac36f4849fe5a2a4afb18661
MilicTG <milic.tg@gmail.com>
2021-09-21 14:36:15 +0200

    Finished video 32

---
+++ app/src/main/java/dev/milic/to_docompose/components/PriorityDropDown.kt
@@ -0,0 +1,102 @@
+package dev.milic.to_docompose.components
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import dev.milic.to_docompose.R
+import dev.milic.to_docompose.data.models.Priority
+import dev.milic.to_docompose.ui.theme.PRIORITY_DROP_DOWN_HEIGHT
+import dev.milic.to_docompose.ui.theme.PRIORITY_INDICATOR_SIZE
+
+@Composable
+fun PriorityDropDown(
+    priority: Priority,
+    onPrioritySelected: (Priority) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+    val angle: Float by animateFloatAsState(
+        targetValue = if (expanded) 180f else 0f
+    )
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(PRIORITY_DROP_DOWN_HEIGHT)
+            .clickable { expanded = true }
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.disabled)
+            ),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Canvas(
+            modifier = Modifier
+                .size(PRIORITY_INDICATOR_SIZE)
+                .weight(weight = 1f)
+        ) {
+            drawCircle(
+                color = priority.color
+            )
+        }
+        Text(
+            modifier = Modifier.weight(weight = 8f),
+            text = priority.name,
+            style = MaterialTheme.typography.subtitle2
+        )
+        IconButton(
+            onClick = { expanded = true },
+            modifier = Modifier
+                .alpha(ContentAlpha.medium)
+                .rotate(degrees = angle)
+                .weight(weight = 1.5f)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.ArrowDropDown,
+                contentDescription = stringResource(R.string.drop_down_arrow_icon)
+            )
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onPrioritySelected(Priority.LOW)
+                }) {
+                PriorityItem(priority = Priority.LOW)
+            }
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onPrioritySelected(Priority.MEDIUM)
+                }) {
+                PriorityItem(priority = Priority.MEDIUM)
+            }
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onPrioritySelected(Priority.HIGH)
+                }) {
+                PriorityItem(priority = Priority.HIGH)
+            }
+        }
+    }
+}
