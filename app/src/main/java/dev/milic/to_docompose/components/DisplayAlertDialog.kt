dd91d95d6451c0001bf0c04ab90578ec68eed0a9
MilicTG <milic.tg@gmail.com>
2021-09-26 22:14:38 +0200

    Finished video 44

---
+++ app/src/main/java/dev/milic/to_docompose/components/DisplayAlertDialog.kt
@@ -0,0 +1,57 @@
+package dev.milic.to_docompose.components
+
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import dev.milic.to_docompose.R
+
+@Composable
+fun DisplayAlertDialog(
+    title: String,
+    message: String,
+    openDialog: Boolean,
+    closeDialog: () -> Unit,
+    onYesClicked: () -> Unit
+) {
+    if (openDialog) {
+        AlertDialog(
+            onDismissRequest = {
+                closeDialog()
+            },
+            title = {
+                Text(
+                    text = title,
+                    fontSize = MaterialTheme.typography.h5.fontSize,
+                    fontWeight = FontWeight.Bold
+                )
+            },
+            text = {
+                Text(
+                    text = message,
+                    fontSize = MaterialTheme.typography.subtitle1.fontSize,
+                    fontWeight = FontWeight.Normal
+                )
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        onYesClicked()
+                        closeDialog()
+                    }
+                ) {
+                    Text(text = stringResource(R.string.yes))
+                }
+            },
+            dismissButton = {
+                OutlinedButton(
+                    onClick = {
+                        closeDialog()
+                    }
+                ) {
+                    Text(text = stringResource(R.string.no))
+                }
+            }
+        )
+    }
+}
