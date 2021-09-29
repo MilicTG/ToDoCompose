d90487d11a983efd2c3948062ae8cfa4a2c318d3
MilicTG <milic.tg@gmail.com>
2021-09-29 22:20:27 +0200

    Finished video 49

--- app/src/main/java/dev/milic/to_docompose/components/PrioriyItem.kt
+++ app/src/main/java/dev/milic/to_docompose/components/PrioriyItem.kt
@@ -24,7 +24,7 @@ fun PriorityItem(priority: Priority) {
         }
         Text(
             text = priority.name,
-            style = Typography.subtitle1,
+            style = Typography.subtitle2,
             color = MaterialTheme.colors.onSurface,
             modifier = Modifier.padding(start = LARGE_PADDING)
         )
